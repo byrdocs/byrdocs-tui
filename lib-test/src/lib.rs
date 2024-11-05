d6333591995d92b6cfe8773ceaba8e1c9d46a22f
cppHusky <cpphusky@outlook.com>
2024-11-05 16:59:20 +0800

    added definition of metadata and adjusted the assets renamed:    assets/06fa27d50ba4a4baae1b665ea48fd677.yml -> .assets/06fa27d50ba4a4baae1b665ea48fd677.yml renamed:    assets/154a930358251fe7bf774dd194bd9a00.yml -> .assets/154a930358251fe7bf774dd194bd9a00.yml renamed:    assets/3d6fa4f7305fb91e9c014e468037310c.yml -> .assets/3d6fa4f7305fb91e9c014e468037310c.yml modified:   Cargo.lock modified:   Cargo.toml modified:   lib-test/Cargo.toml modified:   lib-test/src/lib.rs new file:   metadata/Cargo.toml new file:   metadata/src/definition.rs new file:   metadata/src/lib.rs new file:   metadata/src/md5sum.rs new file:   metadata/src/yml_gen.rs modified:   tui/Cargo.toml

--- lib-test/src/lib.rs
+++ lib-test/src/lib.rs
@@ -7,6 +7,9 @@ mod tests {
 		let config = get_config().unwrap();
 		let expectation = Config {
 			root_dir: String::from(shellexpand::tilde("~/BYRDOCS")),
+			archive_dir: String::from(shellexpand::tilde("~/BYRDOCS/archive")),
+			cache_dir: String::from(shellexpand::tilde("~/BYRDOCS/.cache")),
+			stockpile_dir: String::from(shellexpand::tilde("~/BYRDOCS/stockpile")),
 		};
 		assert_eq!(config, expectation);
 	}
---
+++ metadata/src/lib.rs
@@ -0,0 +1,3 @@
+pub mod definition;
+pub mod md5sum;
+pub mod yml_gen;
