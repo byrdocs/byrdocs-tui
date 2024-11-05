3ee1b10d3aa1d40780d73cdeeb73668a9b7cc96d
cppHusky <cpphusky@outlook.com>
2024-11-05 20:35:54 +0800

    added md5sum part of metadata module modified:   lib-test/src/lib.rs modified:   metadata/Cargo.toml modified:   metadata/src/md5sum.rs

--- lib-test/src/lib.rs
+++ lib-test/src/lib.rs
@@ -1,6 +1,9 @@
 #[cfg(test)]
 mod tests {
+	use std::fs::File;
+
 	use config::{definition::Config, getconf::get_config};
+	use metadata::md5sum::md5sum;
 
 	#[test]
 	fn config() {
@@ -12,5 +15,13 @@ mod tests {
 			stockpile_dir: String::from(shellexpand::tilde("~/BYRDOCS/stockpile")),
 		};
 		assert_eq!(config, expectation);
+	}
+	#[test]
+	fn md5() {
+		let file =
+			File::open(shellexpand::tilde("~/.config/byrdocs/byrconf.yml").into_owned()).unwrap();
+		let md5 = md5sum(file).unwrap();
+		let expectation: u128 = 0x2b181915816194cb3cb82ae809f89dba;
+		assert_eq!(md5, expectation);
 	}
 }
--- metadata/src/md5sum.rs
+++ metadata/src/md5sum.rs
@@ -1,1 +1,17 @@
+use std::{error::Error, fs::File, io::Read};
 
+use crypto::{digest::Digest, md5::Md5};
+
+pub fn md5sum(mut file: File) -> Result<u128, Box<dyn Error>> {
+	let mut data=vec![];
+	file.read_to_end(&mut data)?;
+	println!("{:?}",data);
+	let mut md5 = Md5::new();
+	md5.input(&data);
+	md5.result(&mut data);
+	let mut result: u128 = 0;
+	for i in 0..16 {
+		result = (result << 8) | data[i] as u128;
+	}
+	Ok(result)
+}
